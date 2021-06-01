57599a98a5965f0ab9f1f01d37e69d8e355d0a36
Larisa Kosareva <kos.lv@yandex.com>
2021-06-01 13:18:10 +0200

    decorators, flask

--- flask-hello/decorator_ex.py
+++ flask-hello/decorator_ex.py
@@ -21,3 +21,33 @@ def slow_func():
 
 fast_func()
 slow_func()
+# css decorators
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+    return wrapper
+
+def make_emphasis(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+    return wrapper
+
+def make_underlined(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+    return wrapper
+
+# logging decorator
+def logging_decorator(fn):
+    def wrapper(*args, **kwargs):
+        print(f"You called {fn.__name__}{args}")
+        result = fn(args[0], args[1], args[2])
+        print(f"It returned: {result}")
+    return wrapper
+
+
+@logging_decorator
+def a_function(a, b, c):
+    return a * b * c
+    
+a_function(1, 2, 3)
