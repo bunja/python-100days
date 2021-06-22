6cb2fb2e8caf586795bf2aba8f336ee38f3f0a7e
Larisa Kosareva <kos.lv@yandex.com>
2021-06-22 13:23:44 +0200

    upgraded with contact forms

--- upgraded blog/main.py
+++ upgraded blog/main.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request
 import requests 
 from datetime import datetime
+import smtplib
+
+import os
+
+my_email = os.getenv("MY_EMAIL")
+password = os.getenv("PASSWORD_SMTPLIB")
+to_email = os.getenv("TO_EMAIL")
 
 app = Flask(__name__)
 
@@ -20,14 +27,28 @@ def about():
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
+    
     if request.method == 'POST':
         data = request.form
         print(data["username"])
         print(data["email"])
         print(data["phone"])
         print(data["message"])
-        return "<h1>Successfully sent your message</h1>"
-    return render_template("contact.html")
+
+        name = data["username"]
+        email = data["email"]
+        phone = data["phone"]
+        message = data["message"]
+
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email, to_addrs=to_email,
+                                msg=f"Subject:Hello, darling!\n\n from: {name}\n\n email: {email}\n\n phone: {phone}\n\n Message: {message}")
+
+
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
 
 @app.route("/form-entry", methods=["POST"])
 def receive_data():
