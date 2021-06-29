41e634ac5b661020b43de002bc3bfa86a9a5a472
Larisa Kosareva <kos.lv@yandex.com>
2021-06-29 14:36:08 +0200

    delete record

--- library-start/main.py
+++ library-start/main.py
@@ -35,13 +35,21 @@ def add():
 @app.route('/edit/<int:book_id>', methods=["GET","POST"])
 def edit(book_id):
     book_to_update = Book.query.get(book_id)
-    print("_________________________")
-    print(book_to_update.title)
+    
     if request.method == "POST":
         book_to_update.rating = request.form['rating']
         db.session.commit()
         return redirect(url_for('home')) 
     return render_template('edit.html', book = book_to_update)
+
+@app.route('/delete/<int:book_id>', methods=["GET"])
+def delete(book_id):
+    book_to_delete = Book.query.get(book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home')) 
+    
+
 
 if __name__ == "__main__":
     app.run(debug=True)
