8ba60dc452c6980576035ec65bd522828c2751ed
Larisa Kosareva <kos.lv@yandex.com>
2021-08-27 15:06:44 +0200

    editing blog posts

--- restful-blog/main.py
+++ restful-blog/main.py
@@ -63,9 +63,25 @@ def about():
 def contact():
     return render_template("contact.html")
 
-@app.route("/edit")
-def edit_post():
-    pass
+@app.route("/edit-post/<int:post_id>", methods=['GET','POST'])
+def edit_post(post_id):
+    post = BlogPost.query.get(post_id)
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
+    )
+    if edit_form.validate_on_submit():
+        post.title=edit_form.title.data
+        post.subtitle=edit_form.subtitle.data
+        post.body=edit_form.body.data
+        post.img_url=edit_form.img_url.data
+        post.author=edit_form.author.data
+        db.session.commit()
+        return redirect(url_for("show_post", post_id=post.id))
+    return render_template("make-post.html", form=edit_form, is_edit=True)
 
 @app.route('/new-post', methods=["GET", "POST"])
 def make_post():
