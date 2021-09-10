2841a4e8fb576ed9878710613d2538be8b605574
Larisa Kosareva <kos.lv@yandex.com>
2021-09-10 15:01:19 +0200

    add post form and ckeditor field

--- blog_proj/main.py
+++ blog_proj/main.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, flash, abort, jsonify
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
+from forms import CreatePostForm
+from flask_ckeditor import CKEditor
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
@@ -60,15 +64,33 @@ def contact():
 def add_new_post():
     pass
 
-@app.route("/edit-post/<int:post_id>")
+@app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
 # @admin_only
 def edit_post(post_id):
-    pass
+    post = BlogPost.query.get(post_id)
+    edit_form = CreatePostForm(
+        title = post.title,
+        subtitle = post.subtitle,
+        img_url = post.img_url,
+        body = post.body
+    )
+    if edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.img_url = edit_form.img_url.data
+        
+        post.body = edit_form.body.data
+        db.session.commit()
+        return redirect(url_for("show_post", post_id=post.id))
+
+    return render_template("make-post.html", form=edit_form)
+
 
 @app.route("/delete/<int:post_id>")
 # @admin_only
 def delete_post(post_id):
     pass
+
 
 if __name__ == "__main__":   
     app.run(port=5000, debug=True)
