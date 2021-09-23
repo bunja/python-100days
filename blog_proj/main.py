20021defa22820a7c849d7d39596ea35da9b4b10
Larisa Kosareva <kos.lv@yandex.com>
2021-09-23 15:04:53 +0200

    comment form

--- blog_proj/main.py
+++ blog_proj/main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for, flash, abort, jsoni
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
-from forms import CreatePostForm, RegisterForm, LoginForm
+from forms import CommentForm, CreatePostForm, RegisterForm, LoginForm
 from flask_ckeditor import CKEditor
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -11,6 +11,16 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+#user loader function
+@login_manager.user_loader
+def load_user(user_id):
+    #we are getting user from db
+    return User.query.get(int(user_id))
+
 
 ##CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myblog.db'
@@ -27,7 +37,7 @@ class User(UserMixin, db.Model):
     #This will act like a List of BlogPost objects attached to each User. 
     #The "author" refers to the author property in the BlogPost class.
     posts = relationship("BlogPost", back_populates="author")
-    
+    comments = relationship("Comment", back_populates="comment_author")
 
 class BlogPost(db.Model):
     __tablename__ = "blog_posts"
@@ -40,16 +50,26 @@ class BlogPost(db.Model):
     #Create Foreign Key, "users.id" the users refers to the tablename of User.
     author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     author = relationship("User", back_populates="posts")
+    comments = relationship("Comment", back_populates="parent_post")
+
+class Comment( db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    comment_author = relationship("User", back_populates="comments")
+    post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
+    parent_post = relationship("BlogPost", back_populates="comments")
 
 
-db.create_all()
+# db.create_all()
     
 @app.route('/')
 def get_all_posts():
     posts = BlogPost.query.all()
 
     print(posts)
-    return render_template("schmindex.html", all_posts=posts)
+    return render_template("schmindex.html", all_posts=posts, current_user=current_user)
 
 @app.route('/register', methods=["POST", "GET"])
 def register():
@@ -72,28 +92,59 @@ def register():
         login_user(new_user)
         return redirect(url_for("get_all_posts"))
 
-    return render_template("register.html", form=form)
+    return render_template("register.html", form=form, current_user=current_user)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template("login.html", form=form)
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        user = User.query.filter_by(email=email).first()
+
+        # email doesn't exist
+        if not user:
+            flash("That email doesn't exist. Please try again")
+            return redirect(url_for('login'))
+        # password is incorrect
+        elif not check_password_hash(user.password, password):
+            flash("Password is incorrect. Please try again")
+            return redirect(url_for('login'))
+        # email exists and password is correct
+        else:
+            login_user(user)
+            return redirect(url_for("get_all_posts"))
+    return render_template("login.html", form=form, current_user=current_user)
 
 @app.route('/logout')
+@login_required
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('get_all_posts'))
 
 @app.route("/post/<int:post_id>", methods=["POST", "GET"])
 # @login_required
 def show_post(post_id):
+    form = CommentForm()
     requested_post = BlogPost.query.get(post_id)
-    return render_template("post-second.html", post=requested_post)
+    print(current_user)
+    if form.validate_on_submit():
+        new_comment = Comment(
+            text = form.comment.data,
+            comment_author = current_user,
+            parent_post = requested_post
+        )
+        db.session.add(new_comment)
+        db.session.commit()
+    return render_template("post-second.html", post=requested_post, current_user=current_user, form=form)
 
 @app.route("/about")
+@login_required
 def about():
     pass
 
 @app.route("/contact")
+@login_required
 def contact():
     pass
 
